8a5587027527b5f3f203972fcb9a26eb98df59ea
josh005 <imjbmkz@gmail.com>
2024-03-03 22:38:27 +0800

    Updates for episode 4 (removed FTP connection)

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Hello! This is Josh Dev. In this series, we created an end-to-end data project t
 ## Episodes
 1. Introduction and project overview ([recording](https://youtu.be/S9mVrof-bR8))
 2. Version control and virtual environment essentials for data professionals ([recording](https://youtu.be/F5M4WZifOu0))
-3. Extracting data to FTP using Python ([recording]() | [project](/projects/3.%20Extracting%20data%20from%20web%20to%20FTP%20using%20Python/))
-4. Loading CSV files from FTP to PostgreSQL using SSIS
+3. Extracting data to FTP using Python ([recording](https://youtu.be/j7fNG-V4aGE) | [project](/projects/3.%20Extracting%20data%20from%20web%20to%20FTP%20using%20Python/))
+4. Loading CSV files from FTP to PostgreSQL using SSIS ([recording](https://youtu.be/m2DD-RvT-nA) | [project](/projects/4.%20Loading%20CSV%20files%20from%20FTP%20to%20PostgreSQL%20using%20SSIS/))
 5. Developing screening API using FastAPI
 6. Data modeling and visualization using Power BI
 7. Supervised machine learning and regression analysis primer
--- projects/3. Extracting data from web to FTP using Python/app.py
+++ projects/3. Extracting data from web to FTP using Python/app.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import time
+import concurrent.futures
 import schedule
 import pandas as pd
 from os import environ, remove
@@ -24,42 +25,47 @@ def upload_to_ftp(ftp: FTP_TLS, file_to_upload: str):
 
     # Upload file
     with open(file_path, "rb") as fp:
-        ftp.storbinary(f"STOR {file_name}", fp)
+        ftp.storbinary(f"STOR ftp/{file_name}", fp)
 
 def read_csv(config: dict) -> pd.DataFrame:
     url = config["URL"]
     params = config["PARAMS"]
     return pd.read_csv(url, **params)
 
-def pipeline():
-    # Load source configuration
+def pipeline(source_config: dict):
+    ftp = get_ftp()
+    name = list(source_config.keys())[0]
+    config = source_config[name]
+
+    # Download the file and save as CSV
+    file_name = f"{name}.csv"
+    print(f"Downloading {file_name}.")
+    df = read_csv(config)
+    df.to_csv(file_name,index=False)
+
+    # Upload to FTP
+    upload_to_ftp(ftp, file_name)
+
+    # Delete file
+    remove(file_name)
+
+    print(f"{file_name} has been uploaded to FTP.")
+
+def parallel_execution():
+    
     with open("config.json", "rb") as fp:
         config = json.load(fp)
 
-    ftp = get_ftp()
-
-    # Loop through each source config
-    for name, source in config.items():
-        # Download the file and save as CSV
-        file_name = f"{name}.csv"
-        df = read_csv(source)
-        df.to_csv(file_name,index=False)
-
-        # Upload to FTP
-        upload_to_ftp(ftp, file_name)
-
-        # Delete file
-        remove(file_name)
-
-        print(f"{file_name} has been uploaded to FTP.")
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        executor.map(pipeline, config)
 
 if __name__=="__main__":
 
     if sys.argv[1]=="manual":
-        pipeline()
+        parallel_execution()
 
     elif sys.argv[1]=="schedule":
-        schedule.every().day.at("22:23").do(pipeline)
+        schedule.every().day.at("22:23").do(parallel_execution)
 
         while True:
             schedule.run_pending()
